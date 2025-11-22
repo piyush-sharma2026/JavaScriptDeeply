8d859e473c1062929a1d411c95e9a77acd738cae
Piyush Kumar Sh <piyushdeg9@gmail.com>
2025-11-22 17:21:54 +0000

    Scope and Hoisting in Function

---
+++ 03_basics/03_scopeAndHoisting.js
@@ -0,0 +1,23 @@
+function one() {
+    const username = "Peter"; // function scope
+
+    function two() {
+        const website = "youtube"; // block scope
+        console.log(username); // can access variable from parent scope
+
+    }
+    //  console.log(website); // Error: website is not defined
+    two(); // calling the inner function
+}
+one(); // calling the outer function
+
+
+
+if (true) {
+    const age = 30;
+    if (age === 30) { // or true
+        const name = "John"; // block scope
+        console.log(age); // can access variable from parent scope
+    }
+    //   console.log(name); // Error: name is not defined
+}
