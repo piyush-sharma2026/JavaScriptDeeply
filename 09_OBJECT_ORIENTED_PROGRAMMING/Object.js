e342910036714a8a6c179c958e9ed2c2d00a72d8
Piyush Kumar Sh <piyushdeg9@gmail.com>
2025-12-04 00:16:46 +0000

    Prototypes in JS

---
+++ 09_OBJECT_ORIENTED_PROGRAMMING/Object.js
@@ -0,0 +1,31 @@
+// Protypes
+// Function can work like a function and also it can work like an object
+function multiplyBy5(num) {
+    return num * 5;
+}
+
+multiplyBy5.power = 2;
+
+console.log(multiplyBy5(5));
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype);
+
+/*********************************** */
+
+function createUser(username, score) {
+    this.score = score;
+    this.username = username;
+}
+
+createUser.prototype.increment = function () {
+    this.score++;
+}
+createUser.prototype.printMe = function () {
+    console.log(`Score is ${this.score}`);
+}
+
+const chai = new createUser("chai", 25);
+const tea = new createUser("Tea", 250);
+
+chai.printMe(); // Score is 25
+tea.printMe(); // Score is 250
